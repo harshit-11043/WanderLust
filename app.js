e718e0b11d621fd11fde06eceed998168591d102
Harshit Luthra <harshitluthra123@gmail.com>
2024-02-22 23:20:21 +0530

    Adding Authentication signup/login using passport

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 
 const Review=require("./models/review.js");
-const listings=require("./routes/listing.js");
-const reviews=require("./routes/review.js");
+
+const listingRouter=require("./routes/listing.js");
+const reviewRouter=require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 const flash=require("connect-flash");
 const session=require("express-session");
 app.set("View engine","ejs");
@@ -19,6 +21,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+const passport= require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 async function main()
 {
     await mongoose.connect(MongoURL)
@@ -50,14 +55,30 @@ app.get("/",(req,res)=>
 });
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>
 {
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     next();
 });
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews)
+// app.get("/demouser",async(req,res)=>
+// {
+//      let fakeUser= new User({
+//         email:"abcd@gmail.com",
+//         username:"delta-student",
+//      })
+//      let registeredUser=await User.register(fakeUser,"helloworld");
+//      res.send(registeredUser);
+// })
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 
